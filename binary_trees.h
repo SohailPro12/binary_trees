b9dfb168ec65f3053613f24198d424be83dbed1a
Sohail Charef <charefsohayl@gmail.com>
2024-01-04 10:19:00 -0800

    update

--- binary_trees.h
+++ binary_trees.h
@@ -23,4 +23,7 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
+/*functions*/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
 #endif /* _BINARY_TREES_H_ */
