757216102487c8f57523458982fc11cb848a36ef
Sohail Charef <charefsohayl@gmail.com>
2024-01-04 10:54:56 -0800

    update

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -23,12 +23,14 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	new_node->parent = parent;
 	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
 	if (parent->left)
 	{
-		temp = parent->left;
-		parent->left = new_node;
-		new_node->left = temp;
+		new_node->left = parent->left;
+		new_node->left->parent = new_node;
 	}
-	new_node->right = NULL;
+	parent->left = new_node;
 	return (new_node);
 }
