8a6a9bb2dcee9cc024d63d092ac2cc3d63134b40
Sohail Charef <charefsohayl@gmail.com>
2024-01-05 05:52:13 -0800

    update

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -18,13 +18,9 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (NULL);
 
 	if (parent == NULL)
-	{
-		new_node->parent = NULL;
-	}
-	else
-	{
-		new_node->parent = parent;
-	}
+		return (NULL);
+
+	new_node->parent = parent;
 	new_node->n = value;
 	new_node->left = NULL;
 	new_node->right = NULL;
