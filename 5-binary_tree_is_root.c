d74aa7bd751e1be73d785446cf10dbe9d31ee865
Sohail Charef <charefsohayl@gmail.com>
2024-01-05 06:53:58 -0800

    update

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -13,6 +13,8 @@ int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
+	if (node->parent == NULL)
+		return (1);
 
-	return (node->parent == NULL);	
+	return (0);
 }
